ddd94d372d47ed55a343599ac3573cb6ac922260
MRizkyPradana <147679690+MRizkyPradana@users.noreply.github.com>
2024-06-04 02:05:03 -0700

    Delete PTM 15 directory

--- PTM 15/koneksi_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "login";
-
-$koneksi = mysqli_connect($server, $user, $pass, $database) or die(mysqli_error($konksi));
--- PTM 15/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['username']);
-unset($_SESSION['password']);
-unset($_SESSION['nama_lengkap']);
-unset($_SESSION['level']);
-
-session_destroy();
-echo "<script>alert('Anda Telah Keluar Dari Halaman Admin');document.location='index.php'</script>";
