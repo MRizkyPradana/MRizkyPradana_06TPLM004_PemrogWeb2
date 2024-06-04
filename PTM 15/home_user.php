37075a9418c3d02b137f8ff9de70f10065bd7728
MRizkyPradana <147679690+MRizkyPradana@users.noreply.github.com>
2024-06-04 02:01:14 -0700

    Add files via upload

---
+++ PTM 15/home_user.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if (empty($_SESSION['username']) or empty($_SESSION['level'])) {
+    echo "<script>alert('Maaf, Kawan Untuk Mengakses Halaman Ini, Anda Harus Login Terlebih Dahulu, Terimakasih');
+        document.location='index.php'</script>";
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dasboard Pegawai</title>
+    <!-- Bootstrap core CSS -->
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron bg-info text-white">
+            <h1 class="display-4">Hello, <b><?= $_SESSION['nama_lengkap'] ?></b></h1>
+            <p class="lead">Selamat data, login sebagai <b><?= $_SESSION['level'] ?></b></p>
+            <hr class="my-4">
+            <a class="btn btn-primary btn-lg" href="logout.php" role="button">Logout</a>
+        </div>
+    </div>
+</body>
+
+</html>
